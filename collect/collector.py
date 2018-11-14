421509987d4f27947dd759c096e206c9525ee379
896452534@qq.com <896452534@qq.com>
2018-11-14 15:31:26 +0800

    1. 添加获取城市监测点\n 2. 添加获取当前位置可监测次数

--- collect/collector.py
+++ collect/collector.py
@@ -1,8 +1,13 @@
 # -*- coding:utf-8-*-
 
-import sys
 import json
+import re
+import sys
+
 import requests
+from bs4 import BeautifulSoup
+
+# import ast
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -27,3 +32,40 @@ def get_data(url, code):
         dtime = content['dtime']  # 下载时间
     print "%s\t%s %s\t%s\t%s\t%s" % (status, country, city, rtime, ctime, dtime)
     return (country, city, rtime, ctime, dtime)
+
+
+def checkAvailable():
+    url = "https://api.asm.ca.com/1.6/acct_credits?&callback=check_avail_credits"
+    response = requests.get(url)
+    if response.status_code != 200:
+        return 0
+    # content = json.loads(response.content.replace("check_avail_credits%s(" % (response), "").replace(")", "")).get(
+    #     "result")
+    try:
+        content = json.loads(response.content.replace("check_avail_credits(", "").replace(")", "")).get(
+            "result")
+        result = eval(str(content.get('credits')).replace("[", "").replace("]", "")).get('available')
+        # print ast.literal_eval(str(content.get('credits')).replace("[", "").replace("]", "")).get('available')
+        return result
+    except Exception, e:
+        print e.message
+        return 0
+
+
+def getCity():
+    citys = []
+    url = "https://asm.ca.com/en/checkit.php"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return 0
+    soup = BeautifulSoup(response.content, "html5lib")
+    datas = soup.find_all('span', attrs={"class": "h7"})
+
+    for data in datas:
+        middle = str(data.get_text()).split("-")[1].strip()
+        city = re.sub('\(.*\)', "", middle).strip()
+        # logging.info("当前城市: %s" % (city))
+        citys.append(city)
+
+    return citys
